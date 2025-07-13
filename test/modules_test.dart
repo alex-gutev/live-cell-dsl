b4c9a6c33e6849179a9187b28a5682fe1b485efe
Alexander Gutev <alex.gutev@gmail.com>
2025-07-13 10:56:46 +0200

    Add unit tests for multiple and chained module imports.

--- test/modules_test.dart
+++ test/modules_test.dart
@@ -8,7 +8,7 @@ import 'build_test_utils.dart';
 
 void main() {
   group('Importing all cells defined in module', () {
-    test('Import another module', () => BuildTester(
+    test('Import a single module', () => BuildTester(
         'import(utils);'
         'var(x);'
         'y = inc(x);',
@@ -64,6 +64,202 @@ void main() {
         module: 'utils'
     ).addOperation(SemanticAnalyzer())
      .addOperation(CellFolder())
+        .run());
+
+    test('Import a module that imports another module', () => BuildTester(
+        'import(utils);'
+            'var(x);'
+            'y = inc(x);',
+
+        operators: [
+          Operator(
+              name: '=',
+              type: OperatorType.infix,
+              precedence: 1,
+              leftAssoc: false
+          ),
+          Operator(
+              name: '+',
+              type: OperatorType.infix,
+              precedence: 5,
+              leftAssoc: true
+          )
+        ]
+    ).addModule(
+        name: 'utils',
+        source: 'import(math);'
+            'inc(n) = n + 1;'
+    ).addModule(
+        name: 'math',
+        source: 'external(+(a,b));'
+    ).hasNamed(
+        'x',
+        tester: SpecTester.variable()
+    ).hasApplication(
+        operator: NamedCellId('inc', module: 'utils'),
+        operands: [
+          NamedCellId('x')
+        ],
+
+        tester: SpecTester.apply(
+            operator: SpecTester.ref(
+                NamedCellId('inc', module: 'utils')
+            ),
+
+            operands: [
+              SpecTester.ref(NamedCellId('x'))
+            ]
+        )
+    ).hasNamed('y',
+        tester: SpecTester.ref(
+            AppliedCellId(
+                operator: NamedCellId('inc', module: 'utils'),
+                operands: [NamedCellId('x')]
+            )
+        )
+    ).hasNamed(
+        'inc',
+        module: 'utils'
+    ).hasNamed(
+        '+',
+        module: 'math'
+    ).addOperation(SemanticAnalyzer())
+        .addOperation(CellFolder())
+        .run());
+
+    test('Import multiple modules', () => BuildTester(
+        'import(utils);'
+            'import(math);'
+            'var(x);'
+            'y = inc(x) + 2',
+
+        operators: [
+          Operator(
+              name: '=',
+              type: OperatorType.infix,
+              precedence: 1,
+              leftAssoc: false
+          ),
+          Operator(
+              name: '+',
+              type: OperatorType.infix,
+              precedence: 5,
+              leftAssoc: true
+          )
+        ]
+    ).addModule(
+        name: 'utils',
+        source: 'inc(n) = n;'
+    ).addModule(
+        name: 'math',
+        source: 'external(+(a,b));'
+    ).hasNamed(
+        'x',
+        tester: SpecTester.variable()
+    ).hasApplication(
+        operator: NamedCellId('inc', module: 'utils'),
+        operands: [
+          NamedCellId('x')
+        ],
+
+        tester: SpecTester.apply(
+            operator: SpecTester.ref(
+                NamedCellId('inc', module: 'utils')
+            ),
+
+            operands: [
+              SpecTester.ref(NamedCellId('x'))
+            ]
+        )
+    ).hasNamed('y',
+        tester: SpecTester.ref(
+            AppliedCellId(
+              operator: NamedCellId('+', module: 'math'),
+              operands: [
+                AppliedCellId(
+                    operator: NamedCellId('inc', module: 'utils'),
+                    operands: [NamedCellId('x')]
+                ),
+                ValueCellId(2)
+              ]
+            )
+        )
+    ).hasNamed(
+        'inc',
+        module: 'utils'
+    ).hasNamed(
+        '+',
+        module: 'math'
+    ).addOperation(SemanticAnalyzer())
+        .addOperation(CellFolder())
+        .run());
+
+    test('Import multiple modules multiple times', () => BuildTester(
+        'import(utils);'
+            'import(math);'
+            'var(x);'
+            'y = inc(x) + 2',
+
+        operators: [
+          Operator(
+              name: '=',
+              type: OperatorType.infix,
+              precedence: 1,
+              leftAssoc: false
+          ),
+          Operator(
+              name: '+',
+              type: OperatorType.infix,
+              precedence: 5,
+              leftAssoc: true
+          )
+        ]
+    ).addModule(
+        name: 'utils',
+        source: 'import(math);'
+            'inc(n) = n + 1;'
+    ).addModule(
+        name: 'math',
+        source: 'external(+(a,b));'
+    ).hasNamed(
+        'x',
+        tester: SpecTester.variable()
+    ).hasApplication(
+        operator: NamedCellId('inc', module: 'utils'),
+        operands: [
+          NamedCellId('x')
+        ],
+
+        tester: SpecTester.apply(
+            operator: SpecTester.ref(
+                NamedCellId('inc', module: 'utils')
+            ),
+
+            operands: [
+              SpecTester.ref(NamedCellId('x'))
+            ]
+        )
+    ).hasNamed('y',
+        tester: SpecTester.ref(
+            AppliedCellId(
+                operator: NamedCellId('+', module: 'math'),
+                operands: [
+                  AppliedCellId(
+                      operator: NamedCellId('inc', module: 'utils'),
+                      operands: [NamedCellId('x')]
+                  ),
+                  ValueCellId(2)
+                ]
+            )
+        )
+    ).hasNamed(
+        'inc',
+        module: 'utils'
+    ).hasNamed(
+        '+',
+        module: 'math'
+    ).addOperation(SemanticAnalyzer())
+        .addOperation(CellFolder())
         .run());
   });
 }
